9a99d8a45a8aab851296dc7b13a08f0dc1af2db7
Suniti13 <sunitigaonkar14@gmail.com>
2020-07-14 18:28:37 +0530

    added form class

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import Form, StringField, PasswordField, IntegerField, SubmitField
+from wtforms.validators import Length, DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "123"
+
+class UserForm(FlaskForm):
+    name = StringField('Name', validators = [DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return render_template('base.html')
+    form = UserForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('form.html', form=form)
+
+@app.route('/success')
+def success():
+    return 'Success!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
